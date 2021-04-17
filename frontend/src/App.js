d67d4e4823883d8bd8b8b65eed487c734cd7b045
johnxferraro <johnxferraro@gmail.com>
2021-04-17 02:29:01 -0400

    added HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import HomeScreen from "./Screens/HomeScreen"
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h3>Welcome to MERN Shop</h3>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; MERN Commerce</Col>
+          <Col className="text-center py-3">Copyright &copy; MERN Shop</Col>
         </Row>
       </Container>
     </footer>
