f36811ae76755e6dc772c373ed1cfedb88b062cd
johnxferraro <johnxferraro@gmail.com>
2021-04-17 01:10:45 -0400

    installed bootstrap and setup navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to MERN Commerce</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h3>Welcome to MERN Shop</h3>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
