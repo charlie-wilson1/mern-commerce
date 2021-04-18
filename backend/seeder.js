d02ff6b28ae31bef7abc2dd64b115c68249af9b5
johnxferraro <johnxferraro@gmail.com>
2021-04-18 19:15:57 -0400

    implemented data seeder script

---
+++ backend/seeder.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import colors from "colors"
+import users from "./data/users.js"
+import products from "./data/products.js"
+import User from "./models/userModel.js"
+import Product from "./models/productModel.js"
+import Order from "./models/orderModel.js"
+import connectDB from "./config/db.js"
+
+// get environment variables
+dotenv.config()
+
+// connect to DB
+connectDB()
+
+// import data
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    // insert users into db
+    const createdUsers = await User.insertMany(users)
+
+    // get first user from db array
+    const adminUser = createdUsers[0]._id
+
+    // insert products
+    const sampleProducts = products.map(product => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+    console.log("Data Imported".green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+// destroy data
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log("Data Destroyed".red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+if (process.argv[2] === "-d") {
+  destroyData()
+} else {
+  importData()
+}
