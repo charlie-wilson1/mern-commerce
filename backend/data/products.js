3f3ce55e829a93cca3af2be3130fa6ecb378bb42
johnxferraro <johnxferraro@gmail.com>
2021-04-18 15:56:27 -0400

    prepared users' data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
     image: "/images/airpods.jpg",
     description:
@@ -10,10 +9,9 @@ const products = [
     price: 8999,
     countInStock: 10,
     rating: 4.5,
-    numReviews: 12,
+    numReviews: 12
   },
   {
-    _id: "2",
     name: "iPhone 11 Pro 256GB Memory",
     image: "/images/phone.jpg",
     description:
@@ -23,10 +21,9 @@ const products = [
     price: 59999,
     countInStock: 7,
     rating: 4.0,
-    numReviews: 8,
+    numReviews: 8
   },
   {
-    _id: "3",
     name: "Cannon EOS 80D DSLR Camera",
     image: "/images/camera.jpg",
     description:
@@ -36,10 +33,9 @@ const products = [
     price: 92999,
     countInStock: 5,
     rating: 3,
-    numReviews: 12,
+    numReviews: 12
   },
   {
-    _id: "4",
     name: "Sony Playstation 4 Pro White Version",
     image: "/images/playstation.jpg",
     description:
@@ -49,10 +45,9 @@ const products = [
     price: 39999,
     countInStock: 11,
     rating: 5,
-    numReviews: 12,
+    numReviews: 12
   },
   {
-    _id: "5",
     name: "Logitech G-Series Gaming Mouse",
     image: "/images/mouse.jpg",
     description:
@@ -62,10 +57,9 @@ const products = [
     price: 4999,
     countInStock: 7,
     rating: 3.5,
-    numReviews: 10,
+    numReviews: 10
   },
   {
-    _id: "6",
     name: "Amazon Echo Dot 3rd Generation",
     image: "/images/alexa.jpg",
     description:
@@ -75,8 +69,8 @@ const products = [
     price: 2999,
     countInStock: 0,
     rating: 4,
-    numReviews: 12,
-  },
+    numReviews: 12
+  }
 ]
 
 export default products
