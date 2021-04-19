874bc3d63641af6cfc3dfc3f9ff0a2eb22dfb464
johnxferraro <johnxferraro@gmail.com>
2021-04-19 06:23:01 -0400

    implemented product list reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
+import { productListReducer } from "./reducers/productReducers"
 
 // combine All reducers
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer
+})
 
 // initial state
 const initialState = {}
