046fa48efed091820d071f2312412f4edbb056ea
johnxferraro <johnxferraro@gmail.com>
2021-04-21 15:14:26 -0400

    implemented remove items from cart

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { Row, Col, ListGroup, Image, Form, Button, Card } from "react-bootstrap"
 import Message from "../components/Message"
-import { addToCart } from "../actions/cartActions"
 import { formatPrice } from "../utils/helpers"
+import { addToCart, removeFromCart } from "../actions/cartActions"
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
@@ -21,7 +21,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty])
 
   const removeFromCartHandler = id => {
-    console.log("remove")
+    dispatch(removeFromCart(id))
   }
 
   const checkoutHandler = id => {
