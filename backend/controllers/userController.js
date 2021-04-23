ceeee31a33ed5031a0c761bd4a537aedc404fcb6
johnxferraro <johnxferraro@gmail.com>
2021-04-23 06:55:56 -0400

    added user registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import User from "../models/userModel.js"
 
 /**
  * @desc    Authenticate user & get Token
- * @route   POST /api/user/login
+ * @route   POST /api/users/login
  * @access  Public
  */
 const authUser = asyncHandler(async (req, res) => {
@@ -23,6 +23,41 @@ const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401)
     throw new Error("Invalid email or password")
+  }
+})
+
+/**
+ * @desc    Register a new user
+ * @route   POST /api/users
+ * @access  Public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error("User Already Exist")
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error("Invalid User Data")
   }
 })
 
@@ -47,4 +82,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,31 +5,40 @@ const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: true
     },
     email: {
       type: String,
       required: true,
-      unique: true,
+      unique: true
     },
     password: {
       type: String,
-      required: true,
+      required: true
     },
     isAdmin: {
       type: Boolean,
       required: true,
-      default: false,
-    },
+      default: false
+    }
   },
   {
-    timestamps: true,
+    timestamps: true
   }
 )
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model("User", userSchema)
 
