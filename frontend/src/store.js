80d9d83efde30c1b6c1bcfd5969cc7bacb1573c5
johnxferraro <johnxferraro@gmail.com>
2021-04-24 00:48:52 -0400

    added user login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,21 +6,29 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
+import { userLoginReducer } from "./reducers/userReducers"
 
 // combine All reducers
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
+// local Storage
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : []
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null
+
 // initial state
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 // array of middlewares
