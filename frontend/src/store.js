746db6f1e578dae2bd51cbedb8a155c802ab64f5
johnxferraro <johnxferraro@gmail.com>
2021-04-24 21:24:16 -0400

    user registration functionality completed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,7 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
-import { userLoginReducer } from "./reducers/userReducers"
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers"
 
 // combine All reducers
 const reducer = combineReducers({
@@ -14,6 +14,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 })
 
 // local Storage
