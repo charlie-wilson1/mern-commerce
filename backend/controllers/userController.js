71fd6c3eaec49a2967af512e628504fd446c9dca
johnxferraro <johnxferraro@gmail.com>
2021-05-01 16:39:10 -0400

    added admin user route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -112,4 +112,14 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+/**
+ * @desc    GET all users
+ * @route   GET /api/users
+ * @access  Private/Admin
+ */
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
