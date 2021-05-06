abc6494f43adac510549c41641379a39dcc7cc0f
johnxferraro <johnxferraro@gmail.com>
2021-05-06 12:23:51 -0400

    implemented getUserById and updateUser endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -139,6 +139,49 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
+/**
+ * @desc    GET user by ID
+ * @route   GET /api/users:id
+ * @access  Private/Admin
+ */
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password")
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error("User Not Found")
+  }
+})
+
+/**
+ * @desc    UPDATE user
+ * @route   PUT /api/users/:id
+ * @access  Private/Admin
+ */
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error("User Not Found")
+  }
+})
+
 export {
   authUser,
   registerUser,
@@ -146,4 +189,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
